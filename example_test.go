7b4eea64cf580186c0eceb10dc94ba3a098af46c
Jeremy Jay <jeremy@pbnjay.com>
2021-07-28 10:31:51 -0400

    adding free memory interface for #7
    untested on BSDs and windows, working on darwin and linux

--- example_test.go
+++ example_test.go
@@ -9,3 +9,6 @@ import (
 func ExampleTotalMemory() {
 	fmt.Printf("Total system memory: %d\n", memory.TotalMemory())
 }
+func ExampleFreeMemory() {
+	fmt.Printf("Free system memory: %d\n", memory.FreeMemory())
+}
